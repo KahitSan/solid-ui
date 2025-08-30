4849c533f278eb7afa9187d2273a4f1d4304879c
Luis Edward Miranda <luisedward.miranda@gmail.com>
2025-08-30 18:37:14 +0800

    chore: enable grid by default

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,10 @@
 import type { Preview } from 'storybook-solidjs-vite';
 import { themes } from 'storybook/theming';
-import "../src/styles/globals.css";
+import '../src/styles/globals.css';
 
 const preview: Preview = {
     tags: ['autodocs'],
     parameters: {
-        // automatically create action args for all props that start with "on"
         actions: { argTypesRegex: '^on.*' },
         controls: {
             matchers: {
@@ -17,6 +16,21 @@ const preview: Preview = {
             codePanel: true,
             theme: themes.dark,
         },
+        // how the grid looks
+        backgrounds: {
+            grid: {
+                cellSize: 20,
+                opacity: 0.5,
+                cellAmount: 5,
+                // offset defaults are fine; include only if you want to override
+                // offsetX: 0,
+                // offsetY: 0,
+            },
+        },
+    },
+
+    initialGlobals: {
+        backgrounds: { grid: true },
     },
 };
 
